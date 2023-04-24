cc41b516b1796e7c6e2f93394e893b8a1b40fe88
Nkiriobasi <obasinkiri@gmail.com>
2023-04-24 03:40:52 +0100

    implemented prime number algorithm

---
+++ Math_Algorithms/prime_number.js
@@ -0,0 +1,25 @@
+/* PROBLEM STATEMENT */
+
+/* Given an integer 'n', find the factorial of that integer */
+
+/* Solution */
+const isPrime = (n) => {
+    if(n < 2){
+        return false;
+    }
+
+    for(let i = 2; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(5));
+
+
+// Big-O = O(sqrt(n))
